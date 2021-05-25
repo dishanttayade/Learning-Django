6e3757be973109c905e5814c22ade735811d2fd9
dishanttayade <dishanttayade1@outlook.com>
2021-05-25 12:37:46 +0530

    Learning Django

--- adoptions/models.py
+++ adoptions/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Pet(models.Model):
     SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
     name = models.CharField(max_length=100)
